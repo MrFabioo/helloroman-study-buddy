05a4857fa7c72a13606a713284a5fbbf123c49a0
Kamil Słomiński <kam_slomka@wp.pl>
2022-06-10 15:36:36 +0200

    Add webpage bookmark ADD USER, and create navigation witch react-router-dom

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,20 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Button from "components/atoms/Button/Button";
+import DeleteButton from "components/atoms/DeleteButton/DeleteButton";
 import { StyledAverage, StyledInfo, Wrapper } from "./UsersListItem.styled";
 
 const UsersListItem = ({
   userData: { name, average, attendance = "0%" },
   deleteUser,
 }) => {
-  const showIndex = (index) => alert(`This is student#${index + 1}`);
   return (
     <Wrapper>
       <StyledAverage value={average}>{average}</StyledAverage>
       <StyledInfo>
         <p>
           {name}
-          <Button onClick={() => deleteUser(name)} />
+          <DeleteButton onClick={() => deleteUser(name)} />
         </p>
         <p>attendance: {attendance}</p>
       </StyledInfo>
