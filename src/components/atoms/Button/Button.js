b85b0d3520920d4198298782ce939286cb241a76
Kamil Słomiński <kam_slomka@wp.pl>
2022-07-08 15:30:29 +0200

    Add login poanel with functionality on localstorage

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-  margin: 15px;
+  cursor: pointer;
+  margin: 15px 0;
   padding: ${({ isBig }) => (isBig ? "10px 38px" : "7px 20px")};
   font-size: ${({ isBig, theme: { fontSize } }) =>
     isBig ? fontSize.m : fontSize.s};
